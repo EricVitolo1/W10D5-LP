f421fea2549b0b51aab2c7b011da183022515200
KevinD <kevduong47@gmail.com>
2022-07-15 13:21:13 -0400

    phase 2 solved

--- practice-for-week-10-sql-3-long-practice-main/sql-practice/phase-2.sql
+++ practice-for-week-10-sql-3-long-practice-main/sql-practice/phase-2.sql
@@ -1,2 +1,11 @@
 -- Using subqueries, find the names of the cats whose owners are either George Beatty or Melynda Abshire
 -- Your code here
+
+SELECT cats.name FROM cats
+WHERE cats.id IN (
+    SELECT cat_id FROM cat_owners
+    WHERE cat_owners.owner_id IN (
+        SELECT id FROM owners
+        WHERE owners.first_name = 'George' OR owners.first_name = 'Melynda'
+    )
+);
--- practice-for-week-10-sql-3-long-practice-main/sql-practice/phase-3.sql
+++ practice-for-week-10-sql-3-long-practice-main/sql-practice/phase-3.sql
@@ -1,2 +1,11 @@
 -- Find the name of the cats co-owned by both George Beatty and Melynda Abshire
 -- Your code here
+
+SELECT cats.name FROM cats
+WHERE cats.id IN (
+    SELECT cat_id FROM cat_owners
+    WHERE cat_owners.owner_id IN (
+        SELECT id FROM owners
+        WHERE owners.first_name = 'George' AND owners.first_name = 'Melynda'
+    )
+);
