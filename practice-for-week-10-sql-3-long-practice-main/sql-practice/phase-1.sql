548ff02f9dc85b42c880d5265c91b5efed31dcc4
root <root@DESKTOP-HHJOMI5.localdomain>
2022-07-15 09:56:49 -0700

    phase 1

--- practice-for-week-10-sql-3-long-practice-main/sql-practice/phase-1.sql
+++ practice-for-week-10-sql-3-long-practice-main/sql-practice/phase-1.sql
@@ -1,8 +1,13 @@
 --Insert new cat named "Red" born this year
--- Your code here
+INSERT INTO cats (name, birth_year)
+VALUES ("Red", 2022);
 
 --Assign ownership of new cat to George Beatty using subqueries
--- Your code here
+INSERT INTO cat_owners (owner_id, cat_id)
+VALUES (
+    5,9
+);
 
 --Query to verify INSERTs worked properly
--- Your code here
+SELECT cats.name from cats JOIN cat_owners ON (cats.id = cat_owners.cat_id)
+WHERE cat_owners.owner_id = 5;
